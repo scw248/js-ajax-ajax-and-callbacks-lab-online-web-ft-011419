a44a018de79fcb62210ea0bc85c747fd811eb939
github username <github email address>
2019-04-24 17:06:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-function displayError() {
-  $('#errors').html("I'm sorry, there's been an error. Please try again.")
-}
+$("#search").on('click', searchRepositories);
 
-function searchRepositories() {
-  const searchTerms = $('#searchTerms').val()
-  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
-    const template = Handlebars.compile($('#results-template').html())
-    $('#results').html(template(data))
-  }).fail(() => {
-    displayError()
-  })
-}
-
-function showCommits(el) {
-  const owner = el.dataset.owner
-  const repo = el.dataset.repository
-  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, data => {
-    const template = Handlebars.compile($('#commits-template').html())
-    $('#details').html(template(data))
-  }).fail(() => {
-    displayError()
-  })
-
-}
 
 function handlebarsSetup() {
-  Handlebars.registerPartial("userDetails", $("#user-details-partial").html());
+  Handlebars.registerPartial('userDetails', $('#user-details-partial').html())
 }
 
 $(document).ready(function () {
-  handlebarsSetup();
+  handlebarsSetup()
 });
+
+function showCommits(el) {
+  var owner = el.dataset.owner;
+  var repo = el.dataset.repository;
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, function (results) {
+    var source = $("#commits-template").html();
+    var template = Handlebars.compile(source);
+    $('#details').html(template(results));
+  }).fail(displayError());
+}
+
+function searchRepositories() {
+  var input = $('#searchTerms').val();
+  var src = $("#results-template").html()
+  var template = Handlebars.compile(src)
+  $.get(`https://api.github.com/search/repositories?q=${input}`, results =>
+    $('#results').html(template(results))).fail(displayError())
+}
+
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.");
+}
